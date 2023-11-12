ac111de0c80e9f0ba5c29d8533c5ef83f7942bfb
Dannykhvn <daniel.khvn@gmail.com>
2023-11-11 16:08:39 -0800

    Update runMe.py

--- runMe.py
+++ runMe.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Your project dependencies
-dependencies = ["express","splite3"]
+dependencies = ["express","sqlite3"]
 
 # Install project dependencies
 for dep in dependencies:
